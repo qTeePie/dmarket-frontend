b67f4f19a3b782cfbd1f3d443255cf068259db41
qTeePie <isabel@chaparian.no>
2025-03-30 10:22:46 +0200

    Page NFTGallery ✨ GalleryItems redirects to NFTDisplay page 🚀

---
+++ src/app/nft/[contract]/[tokenId]/page.tsx
@@ -0,0 +1,17 @@
+// /app/nft/[contract]/[tokenId]/page.tsx
+
+import { fetchNFT, fetchCollectionName } from "@/lib/blockchain/nft";
+import { NFTDisplay } from "@/features/nft-gallery";
+
+export default async function NFTPage(props: {
+  params: { contract: string; tokenId: string };
+}) {
+  const { contract, tokenId } = await props.params;
+
+  const [nft, collectionName] = await Promise.all([
+    fetchNFT(contract, Number(tokenId)),
+    fetchCollectionName(contract),
+  ]);
+
+  return <NFTDisplay nft={nft} collectionName={collectionName} />;
+}
