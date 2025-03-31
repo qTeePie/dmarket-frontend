279c261a3839f59f4b06d6ff7d1bb5b3d31db88d
qTeePie <isabel@chaparian.no>
2025-03-31 21:04:57 +0200

    buttons on homepage ✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
+import { Button } from "@/components/atoms";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-10 gap-6 text-center">
-      <h1 className="text-4xl font-bold">DMrkt</h1>
-      <p className="text-lg max-w-xl">
-        The minimal, on-chain NFT marketplace where creators control their
-        royalties.
-      </p>
+    <main className="flex flex-col items-center justify-start min-h-screen mt-24 p-6 gap-16 text-center">
+      <div className="flex gap-8 mt-4">
+        <Link href="/market-approve" className="w-40">
+          <Button className="w-full">List NFT</Button>
+        </Link>
+        <Link href="/list-nfts" className="w-40">
+          <Button className="w-full">Browse NFTs</Button>
+        </Link>
+      </div>
+
+      <div>
+        <h1 className="text-4xl font-bold">DMrkt</h1>
+        <p className="text-lg max-w-xl">
+          The minimal, on-chain NFT marketplace where creators control their
+          royalties.
+        </p>
+      </div>
     </main>
   );
 }
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -2,31 +2,17 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  onClick: () => void;
+  onClick?: () => void;
   children: ReactNode;
-  color?: "primary" | "secondary" | "danger"; // Add more if you want
   className?: string;
 }
 
-export const Button = ({
-  onClick,
-  children,
-  color = "primary", // default
-  className = "",
-}: ButtonProps) => {
-  const baseStyle = "p-2 rounded-lg text-white";
-  const colorStyle =
-    color === "primary"
-      ? "bg-pink-500"
-      : color === "secondary"
-      ? "bg-blue-500"
-      : "bg-red-600";
+export const Button = ({ onClick, children, className = "" }: ButtonProps) => {
+  const baseStyle =
+    "p-2 rounded-lg bg-neutral-800 hover:bg-neutral-700 text-white";
 
   return (
-    <button
-      onClick={onClick}
-      className={`${baseStyle} ${colorStyle} ${className}`}
-    >
+    <button onClick={onClick} className={`${baseStyle} ${className}`}>
       {children}
     </button>
   );
